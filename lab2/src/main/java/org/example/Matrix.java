3cd1a6d9c3044f23b358b4c4d4687dbd5c9eed9c
resepti <nazarevskyi.glib@lll.kpi.ua>
2023-11-04 18:45:33 +0100

    lab 2 step 2

--- lab2/src/main/java/org/example/Matrix.java
+++ lab2/src/main/java/org/example/Matrix.java
@@ -4,4 +4,31 @@ public class Matrix {
     private int rows;
     private int columns;
     private double[][] matrix;
+
+    //конструктор створення пустої матриці
+
+    public Matrix() {
+        this.rows = 0;
+        this.columns = 0;
+        this.matrix = new double[0][0];
+    }
+
+    // конструктор створення матриці заданого розміру
+    public Matrix(int rows, int cols) {
+        this.rows = rows;
+        this.columns = cols;
+        this.matrix = new double[rows][cols];
+    }
+
+    // конструктор копіювання матриці
+    public Matrix(Matrix otherMatrix) {
+        this.rows = otherMatrix.rows;
+        this.columns = otherMatrix.columns;
+        this.matrix = new double[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                this.matrix[i][j] = otherMatrix.matrix[i][j];
+            }
+        }
+    }
 }
